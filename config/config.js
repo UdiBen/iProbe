631721681afc668fd59c84e9f12fe874ed1958e1
udi <udi@sears.co.il>
2014-11-09 20:14:37 +0200

    minor

--- config/config.js
+++ config/config.js
@@ -3,10 +3,10 @@
  */
 var config = {};
 config.elastic = {};
-config.elastic.host = "cruxelastic1:9200";
+config.elastic.host = "databoxelastic1:9200";
 
 config.db = {};
-config.db.host = "cruxdb";
+config.db.host = "proddb1";
 config.db.user = "reader";
 config.db.pwd = "kozel";
 
@@ -14,15 +14,15 @@ config.gb = {};
 config.gb.host = "http://green.prod.global.s.com/gbox/gb/s/data/";
 
 config.mongo = {};
-config.mongo.host = "pegasusmongo1";
+config.mongo.host = "prodmongos1";
 config.mongo.port = "27017";
-config.mongo.user = "admin";
-config.mongo.pwd = "MF123456";
+config.mongo.user = "reader";
+config.mongo.pwd = "kozel";
 
 config.pricingService = {};
 config.pricingService.host = "http://pricing.shopyourway.com/priceservice/onlineprice/json";
 
 config.productDetails = {};
-config.productDetails.host = "http://10.235.44.56:8081/product-details-service/product-details-service/getProductDetails";
+config.productDetails.host = "http://10.235.45.133:8081/product-details-service/product-details-service/getProductDetails";
 
 module.exports = config;
